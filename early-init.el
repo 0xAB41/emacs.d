44642400743ca8de50e6c02ddec0b7f4c6e7d1d8
Abhilash Meesala <mail@abhilashm.me>
2021-09-18 17:24:47 +0530

    Eject from single org file to single el file

--- early-init.el
+++ early-init.el
@@ -2,19 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Defer garbage collection
-(setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.5)
-
-;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
-
-;;(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
-
 (setq package-enable-at-startup nil)
-
-(setq frame-inhibit-implied-resize t)
 
 (provide 'early-init)
 ;;; early-init.el ends here
